f1628e3ec461822fabcd17758019dcd9214be223
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-17 00:43:24 +0100

    write js file to map platform and arch to available binary file

---
+++ npm-builder/assets/bin/executables.js
@@ -0,0 +1,30 @@
+/**
+ *  Exports a map with all binary variants (platfrom and arch) currently supported by this pacakge
+ * platform === process.platform
+ * arch === process.arch
+ * key of map: `${platform}-${arch}` for faster lookup
+ */
+
+export const executables = new Set([
+    ['win32-x64', {
+        // the path to the executable, relative to the root of the package
+        executable: "windows/concurrently.exe",
+    }], 
+    ['dawin-x64', {
+        executable: "darwin/concurrently",
+    }],
+    // TODO: Compile on Mac Mini M1
+    // ['darwin-arm', {
+    //     executable: "darwin-arm/concurrently",
+    // }],
+    // TODO: Cross-Compile not working yet
+    // ['linux-x64', {
+    //     executable: "linux/concurrently",
+    // }],
+    // ['linux-arm', {
+    //     executable: "linux-arm/concurrently",
+    // }],
+    // ['linux-arm64', {
+    //     executable: "linux-arm64/concurrently",
+    // }],
+])
