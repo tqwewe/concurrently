c4ad81cd112a55417a8ca449ba53a8570d458f3b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-17 01:26:23 +0100

    add assetcopying to main task

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -1,6 +1,14 @@
 // This script file should be run with deno to generate the npm package output
 // deno run --allow-run --allow-read=Cargo.toml npm-builder/build.ts
 
+import { copyAssets } from "../npm-builder/copyAssets.task.ts";
 import { generatePackageJson } from "../npm-builder/packageJson.template.ts";
+import { ensureDir } from "https://deno.land/std@0.168.0/fs/mod.ts";
+import { join } from "https://deno.land/std@0.168.0/path/mod.ts";
 
-await generatePackageJson();
+const outPath = "dist/";
+await ensureDir(outPath);
+await ensureDir(join(outPath, "bin"));
+
+await generatePackageJson(outPath);
+await copyAssets(outPath);
