0b3fc3f9b63f37cbbdd2515a000f0d17b352ce43
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-19 21:44:38 +0100

    add copy task for concurrently binaries

--- npm-builder/copyAssets.task.ts
+++ npm-builder/copyAssets.task.ts
@@ -8,15 +8,14 @@ import { ensureDir } from "https://deno.land/std@0.168.0/fs/mod.ts";
  * Note: Paths are measured from root of repo
  */
 export async function copyAssets(outPath?: string) {
+  // make sure, that the outPath exists
   if (!outPath) {
     outPath = `dist/`;
   }
-
   await ensureDir(outPath);
   await ensureDir(join(outPath, "bin"));
 
   const sourcePath = "./npm-assets";
-
   await Deno.copyFile(join(sourcePath, "index.js"), join(outPath, "index.js"));
   await Deno.copyFile(
     join(sourcePath, "bin/run.mjs"),
@@ -30,6 +29,18 @@ export async function copyAssets(outPath?: string) {
   await Deno.copyFile(
     join(sourcePath, "bin/executables.mjs"),
     join(outPath, "bin/executables.mjs"),
+  );
+
+  // Copy release rust binaries from target folder
+  await (ensureDir(join(outPath, "bin/darwin")));
+  await (ensureDir(join(outPath, "bin/windows")));
+  await Deno.copyFile(
+    `target/release/concurrently`,
+    join(outPath, "bin/darwin/concurrently"),
+  );
+  await Deno.copyFile(
+    `target/x86_64-pc-windows-gnu/release/concurrently.exe`,
+    join(outPath, "bin/windows/concurrently.exe"),
   );
 
   console.info(`Copied assets!`);
